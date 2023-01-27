a23df43ad77c0f0f311213897692b23018f01f6c
ariyan-islam <afislam57@gmail.com>
2023-01-27 17:23:35 +0600

    modify slider

--- src/Pages/HomePage/Slider.js
+++ src/Pages/HomePage/Slider.js
@@ -38,38 +38,38 @@ const Slider = () => {
         >
           <SwiperSlide>
             <div className="sliderContainer w-full h-full">
-              <div className="contents text-3xl lg:text-5xl font-bold">
-                <div className="imgArea relative h-full">
+              <div className="sliderContents h-full text-3xl lg:text-5xl font-bold">
+                <div className="imgArea h-full w-full relative">
                   <img src={sliderImg3} alt="" />
-                </div>
-                <div className="title absolute top-[-40%] left-[5%] lg:top-[10%] lg:left-[2%]">
-                  <p className='text-base-100 mt-32 ml-5'>BOOST YOUR</p>
-                  <p className='text-neutral'>RIDE</p>    
+                    <div className="title absolute top-[20%] left-[5%]">
+                      <p className='text-base-100'>BOOST YOUR</p>
+                      <p className='text-neutral'>RIDE</p>    
+                    </div>
                 </div>
               </div>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div className="sliderContainer w-full h-full">
-              <div className="contents text-3xl font-bold lg:text-7xl text-base-100">
-                <div className="imgArea relative h-full">
+              <div className="sliderContents h-full text-3xl lg:text-7xl font-bold">
+                <div className="imgArea h-full w-full relative">
                   <img src={sliderImg} alt="" />
-                </div>
-                <div className="title absolute top-[45%] left-[25%] lg:top-[50%] lg:left-[30%] ">
-                <p>GO ALL WAYS</p>
+                    <div className="title absolute top-[50%] left-[30%]">
+                      <p className='text-base-100'>GO ALL WAYS</p>
+                    </div>
                 </div>
               </div>
             </div>
           </SwiperSlide>
           <SwiperSlide>
           <div className="sliderContainer w-full h-full">
-            <div className="contents text-3xl font-bold lg:text-7xl text-base-100">
-              <div className="imgArea relative h-full">
-                <img src={sliderImg2} alt="" />
-              </div>
-              <div className="title absolute top-[40%] left-[25%] lg:top-[40%] lg:left-[30%] ">
-                <p>THIS SHIFTS</p>
-                <p className='my-2'>EVERYTHING</p>  
+            <div className="sliderContents h-full text-3xl lg:text-7xl font-bold">
+              <div className="imgArea h-full w-full relative">
+                  <img src={sliderImg2} alt="" />
+                  <div className="title absolute top-[40%] left-[30%] text-base-100">
+                    <p>THIS SHIFTS</p>
+                    <p className='my-2'>EVERYTHING</p>  
+                  </div>
               </div>
             </div>
           </div>
